74a21e063e6a84b359f364b231f2aa4833ff8f53
subrata797979 <subrata797979@gmail.com>
2022-05-30 16:25:31 +0530

    user can be fetched successfully

--- src/app/app-search/app-search.component.ts
+++ src/app/app-search/app-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from "../services/github.service";
 
 @Component({
   selector: 'app-app-search',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppSearchComponent implements OnInit {
 
-  constructor() { }
+  public searchString: string = "";
+  users!: any[];
 
+  // extra
+  public count: number = 1;
+  public storeTime: number = 0;
+  private storeDate: any;
+
+  constructor(private githubService: GithubService) { }
   ngOnInit(): void {
   }
 
+  public getUsers() {
+    this.githubService.getData(this.searchString).subscribe((data) => {
+      this.users = data.items;
+      console.log(this.users)
+    })
+  }
+
+  search() {
+    const now = this.timeToMinutes(new Date());
+    // console.log(now)
+    // console.log(this.storeTime+60)
+    if(this.count==8) {
+      this.storeDate = new Date();
+      this.storeTime = this.timeToMinutes(this.storeDate)
+    }
+    if(this.count>8) {
+      if(this.storeTime!=0 && this.storeTime+60>now) {
+        const timeDiff = this.storeTime+60-now;
+        console.log("wait for "+timeDiff+" seconds as 8 request/minute allowed only");
+      }
+      else {
+        this.count=1;
+        this.getUsers()
+      }
+    }
+    else {
+      this.getUsers()
+    }
+    console.log(this.count)
+    this.count++;
+  }
+
+  // extra
+  timeToMinutes(time: any):number {
+    const hours = (new Date(time)).getHours() * 60;
+    const minutes = (new Date(time)).getMinutes()*60;
+    const seconds = (new Date(time)).getSeconds();
+    time = hours + minutes + seconds;
+    return time;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMainComponent } from './app-main/app-main.component';
 import { AppSearchComponent } from './app-search/app-search.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { AppUsersComponent } from './app-users/app-users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppMainComponent,
-    AppSearchComponent
+    AppSearchComponent,
+    AppUsersComponent
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpClientModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
