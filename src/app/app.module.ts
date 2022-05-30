88a2be588920d518a87f3b0dec6248fa66470326
subrata797979 <subrata797979@gmail.com>
2022-05-30 10:17:58 +0530

    app-search added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMainComponent } from './app-main/app-main.component';
+import { AppSearchComponent } from './app-search/app-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppMainComponent,
+    AppSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
